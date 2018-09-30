01436f4fc253a4340e7c4272be0cb5cce2b3d8b2
Telmo Matos <matos.telmo@gmail.com>
2018-09-30 12:55:04 +0100

    Resolução FP01

---
+++ fp01.py
@@ -0,0 +1,36 @@
+#Ex 1
+"""x =[]
+for i in range(21):
+    x.append(i+5)
+"""
+
+#Ex 2
+"""x=[]
+soma = 0
+n = input('Input number: ')
+while n != '-1' and len(x) < 10:
+    x.append(n)
+    soma = soma + int(n)
+    n = input('Input number: ')
+print('Average = ' , soma/len(x))
+"""
+
+#Ex 3
+"""x=[]
+while len(x) < 10:
+    n = input('Input number: ')
+    x.append(n)
+c = input('Odd(P) or Even(I)?: ')
+odd = 0
+if c == 'I':
+    odd = 1
+for i in range(len(x)):
+    if int(x[i]) % 2 == odd:
+            print(x[i])
+"""
+
+#Ex 4
+#Ex 5
+#Ex 6
+#Ex 7
+#Ex 8
