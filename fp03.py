0b32b4e5f1d6096297a6bba0918b5b3eba409fe1
Telmo Matos <matos.telmo@gmail.com>
2018-10-08 17:13:53 +0100

    Adicionado Exercício 8

--- fp03.py
+++ fp03.py
@@ -49,3 +49,35 @@ for x in p.open():
     #print(s)
 print(sum)
 """
+
+#Ex7
+"""from pathlib import Path
+p = Path('c:/Users/RAMP/Desktop/fp03_exercicios/testeDir_1')
+p.mkdir()
+p = Path('c:/Users/RAMP/Desktop/fp03_exercicios/testeDir_2')
+p.mkdir()
+p = Path('c:/Users/RAMP/Desktop/NovoDir_1/')
+p.mkdir()
+p = Path('c:/Users/RAMP/Desktop/NovoDir_1/testeDir_1')
+p.mkdir()
+
+p = Path('c:/Users/RAMP/Desktop/fp03_exercicios/')
+q = Path('c:/Users/RAMP/Desktop/NovoDir_1/')
+for x in p.iterdir():
+    if x.is_dir():
+        for y in q.iterdir():
+            if y.is_dir():
+                if x.stem == y.stem:
+                    print(x)
+"""
+
+#Ex8
+"""from pathlib import Path
+f = open('c:/Users/RAMP/Desktop/NovoDir_1/OddNumbers.txt', 'w')
+valor = input('introduza uma valor inteiro: ')
+while int(valor) < 1:
+    valor = input('introduza uma valor inteiro: ')
+for i in range(int(valor)+1):
+    if i%2 == 0: 
+        f.write("%s\n" % i)
+"""        
